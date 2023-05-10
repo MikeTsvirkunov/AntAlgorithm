78ef93248a0299fdda042f7fe472106ca13ba7c8
MikeTsvirkunov <mike.tsvirkunov@gmail.com>
2023-05-11 01:24:42 +0600

    Web

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('HW')
+from flask import Flask, render_template, url_for, make_response
+
+app = Flask(__name__)
+
+@app.route('/index', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
